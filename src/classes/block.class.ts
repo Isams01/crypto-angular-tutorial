95a2d59f08926fa0aa3ad5524acc9c7e72c9b25a
Sam Ito <isamsito@gmail.com>
2021-05-05 19:11:05 -0600

    add nonce to verify hash

--- src/classes/block.class.ts
+++ src/classes/block.class.ts
@@ -1,3 +1,4 @@
+import { withModule } from '@angular/core/testing';
 import * as SHA256 from 'crypto-js/sha256';
 
 export class Block{
@@ -6,6 +7,7 @@ export class Block{
   data: any;
   previousHash: string = '';
   hash: string = '';
+  nonce: number = 0;
 
   constructor(index: number, timestamp: any, data: any, previousHash: string) {
     this.index = index;
@@ -16,7 +18,19 @@ export class Block{
   }
 
   calculateHash(){
-    return SHA256( this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256( this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+  }
+
+  mineBlock( difficulty: any ): Promise<any> {
+    let promise = new Promise( (resolve, reject ) => {
+      while ( this.hash.substring(0, difficulty) != Array(difficulty + 1).join( "0" ) ) {
+        this.nonce++
+        this.hash = this.calculateHash();
+      }
+      console.log("Block successfully hashed (" + this.nonce + " iterations) Hash: " + this.hash);
+      resolve(promise);
+    });
+    return promise;
   }
 
 }
