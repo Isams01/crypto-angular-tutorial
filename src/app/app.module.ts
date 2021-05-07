cb53e2e7ad7c95fa449916384c5f2663a89d1545
Sam Ito <isamsito@gmail.com>
2021-05-06 18:21:14 -0600

    add my-materuals module for style imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CryptoService } from './services/crypto.service'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MyMaterialModule } from './modules/my-material/my-material.module'
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MyMaterialModule
   ],
   providers: [CryptoService],
   bootstrap: [AppComponent]
