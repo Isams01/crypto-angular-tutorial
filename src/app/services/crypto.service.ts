86ae24cf48bbe7c4836c7bb27b5faebdcb9ad7a7
Sam Ito <isamsito@gmail.com>
2021-05-08 19:35:01 -0600

    Replace contructor with auto activity

--- src/app/services/crypto.service.ts
+++ src/app/services/crypto.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Blockchain } from '../../classes/blockchain.class';
 import { Block } from '../../classes/block.class';
 import { Transaction } from '../../classes/transaction.class'
-import {} from "../../classes/auto-activity.class"
+import { AutoActivity } from "../../classes/auto-activity.class"
 
 
 @Injectable()
@@ -10,23 +10,27 @@ export class CryptoService {
 
   cryptoChain = new Blockchain();
   unminedTxns: Transaction[] = [];
+  autoActivity: AutoActivity;
 
 
   constructor() {
-    // nothing happens until these transactions are mined
-    // transaction 1
-    this.unminedTxns.push( new Transaction( Date.now(), 'wallet-Alice', 'wallet-Bob', 50) );
+    // // nothing happens until these transactions are mined
+    // // transaction 1
+    // this.unminedTxns.push( new Transaction( Date.now(), 'wallet-Alice', 'wallet-Bob', 50) );
 
-    // transaction 2
-    this.unminedTxns.push( new Transaction( Date.now(), 'wallet-Bob', 'wallet-Alice', 25) );
+    // // transaction 2
+    // this.unminedTxns.push( new Transaction( Date.now(), 'wallet-Bob', 'wallet-Alice', 25) );
 
-    console.log("\nMining a block...");
-    this.cryptoChain.mineCurrentBlock( 'wallet-Miner49r', this.unminedTxns )
-      .then( () => {
-        console.log("\nBalance: Alice: ", + this.cryptoChain.getAddressBalance('wallet-Alice'));
-        console.log("\nBalance: Bob: ", + this.cryptoChain.getAddressBalance('wallet-Bob'));
-        console.log("\nBalance: Miner49r: ", + this.cryptoChain.getAddressBalance('wallet-Miner49r'));
-      });
+    // console.log("\nMining a block...");
+    // this.cryptoChain.mineCurrentBlock( 'wallet-Miner49r', this.unminedTxns )
+    //   .then( () => {
+    //     console.log("\nBalance: Alice: ", + this.cryptoChain.getAddressBalance('wallet-Alice'));
+    //     console.log("\nBalance: Bob: ", + this.cryptoChain.getAddressBalance('wallet-Bob'));
+    //     console.log("\nBalance: Miner49r: ", + this.cryptoChain.getAddressBalance('wallet-Miner49r'));
+    //   });
+
+
+    this.autoActivity = new AutoActivity( this.cryptoChain );
 
    }
 }
